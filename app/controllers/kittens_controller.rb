dd203401ca5242cd5b027151f3dae35d26d76f46
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-19 13:48:27 -0700

    Generate Kitten model and controller.

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,31 @@
+class KittensController < ApplicationController
+  before_action :set_kitten, only: [:show, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def set_kitten
+    end
+
+    def kitten_params
+    end
+end
