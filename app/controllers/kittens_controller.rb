4aef022ed4c00bb587a795849c07247cae8557f4
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-01-19 13:54:53 -0700

    Migrate kitten tables.

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,19 +1,22 @@
 class KittensController < ApplicationController
-  before_action :set_kitten, only: [:show, :edit, :update, :destroy]
+  before_action :set_kitten, only: %i[show edit update destroy]
 
   def index
+    @kittens = Kitten.all
   end
 
   def show
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def edit
   end
 
   def create
+    @kitten = Kitten.create(kitten_params)
   end
 
   def update
@@ -23,9 +26,12 @@ class KittensController < ApplicationController
   end
 
   private
-    def set_kitten
-    end
 
-    def kitten_params
-    end
+  def set_kitten
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
 end
